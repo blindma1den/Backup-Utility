5490d1bee57506b30abc4f0ba37a80c8f0f5da67
Leandro Escalante <gotanegrale@gmail.com>
2024-01-19 11:09:03 -0500

    Informe creado

--- backup-utility.sh
+++ backup-utility.sh
@@ -6,11 +6,15 @@ origen="./filesToBackup"
 # Directorio de destino donde se almacenarán las copias de seguridad
 destino="./backup"
 
+host="192.168.0.200"
+
 # Nombre del archivo de copia de seguridad con marca de tiempo
 archivo_destino="$destino/respaldo_$(date +\%Y\%m\%d_\%H\%M\%S).tar.gz"
 
 # Comprimir y copiar los archivos de origen al directorio de destino
-tar -czvf "$archivo_destino" "$origen"
+tar -czvf "$archivo_destino" "$origen" 
+
+curl -T $archivo_destino --user testUser:mostsecurepassword ftp://192.168.0.200
 
 # Comprobar si la copia de seguridad se realizó con éxito
 if [ $? -eq 0 ]; then
